bfc8cc09c9395a92247ff9ba0c87ea7537593e3c
ikaru19 <syafrizalizal17@gmail.com>
2023-02-02 15:05:40 +0700

    Add Presentation Layer DI

--- Sequis/DI/Module/CoreModule.swift
+++ Sequis/DI/Module/CoreModule.swift
@@ -15,5 +15,7 @@ struct CoreModule: Module {
 
         binder.bind(InjectorResolver.self)
                 .to(factory: MainInjectorResolver.init)
+        binder.bind(ViewControllerResolver.self)
+                .to(factory: MainViewControllerResolver.init)
     }
 }
